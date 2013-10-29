9ef36ad158262cb12482fe2e05989f27380f0460
Matthew Flatt <mflatt@racket-lang.org>
2013-10-29 09:36:11 -0400

    declare package dependencies

--- info.rkt
+++ info.rkt
@@ -1,3 +1,15 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+(define deps '("errortrace-lib"
+               "gui-lib"
+               "slideshow-lib"
+               "base"))
+
+(define build-deps '("draw-doc"
+                     "draw-lib"
+                     "pict-doc"
+                     "racket-doc"
+                     "scribble-lib"
+                     "slideshow-doc"))
